7204827a2b46a243e1a0581b2c8fd0b4971a1e3b
Kuromook <sh1@dream.jp>
2019-08-18 15:26:39 +0900

    csv style output

--- cash.py
+++ cash.py
@@ -1,3 +1,5 @@
+from pprint import pprint as pp
+
 a = {"year":2019, "month":8, "balance":600000}
 
 current = []
@@ -17,26 +19,49 @@ print(r"result current: ", current_balance)
 planA = []
 planA.append({'year':2019, 'month':9, 'income':100000, 'item':'personal'})
 planA.append({'year':2019, 'month':9, 'income':200000, 'item':'publisher'})
-planA.append({'year':2019, 'month':(11,12), 'income':200000, 'item':'publisher'})
-planA.append({'year':2019, 'month':(11,12), 'income':100000, 'item':'personal'})
+planA.append({'year':2019, 'month':12, 'income':200000, 'item':'publisher'})
+planA.append({'year':2019, 'month':12, 'income':100000, 'item':'personal'})
 planA.append({'year':2019, 'month':10, 'income':150000, 'item':'personal'})
-planA.append({'year':2019, 'month':(11,12), 'income':80000, 'item':'dojin'})
+planA.append({'year':2019, 'month':12, 'income':80000, 'item':'dojin'})
 planA.append({'year':2019, 'month':10, 'expence':60000, 'item':'print'})
-planA.append({'year':2019, 'month':(11,12), 'expence':160000, 'item':'cost'})
-planA.append({'year':2019, 'month':(11,12), 'expence':77600*4, 'item':'land'})
-planA.append({'year':2019, 'month':(11,12), 'expence':70000*4, 'item':'life'})
+planA.append({'year':2019, 'month':12, 'expence':160000, 'item':'cost'})
+planA.append({'year':2019, 'month':12, 'expence':77600*4, 'item':'land'})
+planA.append({'year':2019, 'month':12, 'expence':70000*4, 'item':'life'})
 
 print(r"result A: ", result_balance(current_balance, planA))
 
 planB = []
 planB.append({'year':2019, 'month':9, 'income':100000, 'item':'personal'})
 planB.append({'year':2019, 'month':9, 'income':200000, 'item':'publisher'})
-planB.append({'year':2019, 'month':(11,12), 'income':300000, 'item':'personal'})
+planB.append({'year':2019, 'month':12, 'income':300000, 'item':'personal'})
 planB.append({'year':2019, 'month':10, 'income':150000, 'item':'personal'})
-planB.append({'year':2019, 'month':(11,12), 'income':80000, 'item':'dojin'})
+planB.append({'year':2019, 'month':12, 'income':80000, 'item':'dojin'})
 planB.append({'year':2019, 'month':10, 'expence':60000, 'item':'print'})
-planB.append({'year':2019, 'month':(11,12), 'expence':160000, 'item':'cost'})
-planB.append({'year':2019, 'month':(11,12), 'expence':77600*4, 'item':'land'})
-planB.append({'year':2019, 'month':(11,12), 'expence':70000*4, 'item':'life'})
+planB.append({'year':2019, 'month':12, 'expence':160000, 'item':'cost'})
+planB.append({'year':2019, 'month':12, 'expence':77600*4, 'item':'land'})
+planB.append({'year':2019, 'month':12, 'expence':70000*4, 'item':'life'})
 
 print(r"result B: ", result_balance(current_balance, planB))
+
+def output_result(plan):
+    def get_culumn_names(plan):
+        date_keys = list(set([i['month'] for i in plan]))
+        item_keys = list(set([ i['item'] for i in plan]))
+        return (date_keys, item_keys)
+
+    def val(plan, i,m):
+        for p in plan:
+            if p['item'] == i and p['month'] == m:
+                if 'income' in p:
+                    return p['income']
+                elif 'expence' in p:
+                    return p['expence'] * (-1)
+        return 0
+
+    header, row = get_culumn_names(plan)
+    result = [['row'] + header] + [[r] + [val(plan, r, m)  for m in header] for r in row]
+
+    return result
+
+pp(output_result(planA))
+pp(output_result(planB))
