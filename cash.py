ea74998077f88812e0cb137af9f01ce304303945
Kuromook <sh1@dream.jp>
2019-09-22 17:18:54 +0900

    group パラメータの追加

--- cash.py
+++ cash.py
@@ -5,7 +5,6 @@ import yaml
 level = 2
 input_file = 'test.yaml'
 
-
 with open(input_file) as f: obj = yaml.load(f, Loader=yaml.SafeLoader)
 
 div = obj['div']
@@ -13,14 +12,17 @@ start = obj['start']
 current = obj['current']
 planA = obj['planA']
 planB = obj['planB']
+group = obj['group']
 
-for a in current: a['amount'] = eval(a['amount']) if   isinstance(a['amount'], str) else a['amount']
-for a in planA: a['amount'] = eval(a['amount']) if   isinstance(a['amount'], str) else a['amount']
-for a in planB: a['amount'] = eval(a['amount']) if   isinstance(a['amount'], str) else a['amount']
+''''数値の文字列表現を演算・タプルの文字列表現をタプルに変換'''
+for a in current: a['amount'] = eval(a['amount']) if isinstance(a['amount'], str) else a['amount']
+for a in planA: a['amount'] = eval(a['amount']) if isinstance(a['amount'], str) else a['amount']
+for a in planB: a['amount'] = eval(a['amount']) if isinstance(a['amount'], str) else a['amount']
 
-for a in current: a['month'] = eval(a['month']) if   isinstance(a['month'], str) else a['month']
-for a in planA: a['month'] = eval(a['month']) if   isinstance(a['month'], str) else a['month']
-for a in planB: a['month'] = eval(a['month']) if   isinstance(a['month'], str) else a['month']
+for a in current: a['month'] = eval(a['month']) if isinstance(a['month'], str) else a['month']
+for a in planA: a['month'] = eval(a['month']) if isinstance(a['month'], str) else a['month']
+for a in planB: a['month'] = eval(a['month']) if isinstance(a['month'], str) else a['month']
+
 
 def result_balance(balance, plan):
     '''planを適用し、将来の結果を取得'''
@@ -45,17 +47,18 @@ def output_result(plan, level=1):
     # 月の表現を tuple で統一化
     for p in plan: p["month"] = (p["month"],) if isinstance(p["month"], int) else p["month"] 
 
+    mlist = list(set([(p['year'],  p['month']) for p in plan]))
+    mlist.sort()
+    dic = {}
+    newplan = []
+    for m in mlist:
+        dic[m] = [p for p in plan if m[0] == p['year'] and m[1] == p['month']]
+
     if level==1:
-        mlist = list(set([(p['year'],  p['month']) for p in plan]))
-        mlist.sort()
-        d = {}
-        newplan = []
         for m in mlist:
-            d[m] = [p for p in plan if m[0] == p['year'] and m[1] == p['month']]
-        for m in mlist:
-            p_cost = [p['amount'] for p in d[m] if div[p['item']]['parent'] == 'cost']
+            p_cost = [p['amount'] for p in dic[m] if div[p['item']]['parent'] == 'cost']
             newplan.append({'year':m[0], 'month':m[1], 'item':'cost', 'class': 'expence', 'amount':sum(p_cost)})
-            p_sales = [p['amount'] for p in d[m] if div[p['item']]['parent'] == 'sales']
+            p_sales = [p['amount'] for p in dic[m] if div[p['item']]['parent'] == 'sales']
             newplan.append({'year':m[0],'month':m[1], 'item':'sales', 'class':'income','amount':sum(p_sales)})
         plan = newplan
 
